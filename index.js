679f38ccd6ed9bb4e6c6ad6afc7ff14bd6b6dfcd
Sabine Rosenberg <11468543+anomalse@users.noreply.github.com>
2024-02-05 15:41:58 -0500

    added

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const express = require("express");
 // Initialize Express
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+
+  //note how the route NEED not be the same as the page that will be served! 
+  app.get('/start',requestHandlerStart);
+
+  function requestHandlerStart(request,response){
+    response.sendFile(__dirname + '/public/start.html');
+}
 
 // Initialize server
 app.listen(5000, () => {
