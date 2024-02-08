3535ea819c092bfeacb77991fa04f3752baed1bd
Sabine Rosenberg <11468543+anomalse@users.noreply.github.com>
2024-02-08 17:25:35 -0500

    hmmmm

--- api/index.js
+++ api/index.js
@@ -1,6 +1,26 @@
 /*https://vercel.com/guides/using-express-with-vercel*/
+/** to try::
+ * https://jonathans199.medium.com/deploy-node-js-express-api-to-vercel-dbf4461795a5
+ */
 const app = require('express')();
-const { v4 } = require('uuid');
+const handleUpload  = require('@vercel/blob/client');
+
+//const fileUpload = require('express-fileupload');
+//app.use(fileUpload());
+// for the client...
+
+
+
+// Note that this option available for versions 1.0.0 and newer. 
+// app.use(fileUpload({
+//     useTempFiles : true,
+//     tempFileDir : '/tmp/'
+// }));
+// app.use((req, res, next) => {
+//     //res.header('Access-Control-Allow-Origin', '*');
+//     //next();
+//     console.log(req);
+//   });
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -14,9 +34,15 @@ app.get('/api/item/:slug', (req, res) => {
   console.log(req.query);
   switch(slug){
     case 'varsArePassing':
+        /* GET REQUEST EX */
        // res.end(`query: ${req.query.fruits} and ${req.query.vegs} `)
         res.send({"fruit": req.query.fruits,"veg":req.query.vegs});
         break;
+    
+    // case 'upload_file_post':
+    //      /* POST REQUEST EX */
+    //        res.end(`testing post 123`)
+    //     break;
     default:
         res.end(`no case `);
 
@@ -24,16 +50,85 @@ app.get('/api/item/:slug', (req, res) => {
  
 });
 
-//  app.get('/start', requestRootHandler);
+// export async function POST(request) {
+//     const body = await request.json();
+//     console.log(body);
+// }  
 
-// function requestRootHandler(request,response){
-//     console.log("running");
-//     response.end('test 123');
-//  }
-  
-
-
-
-
-
+app.post('/api/item/:slug',async(req, res) => {
+      const { slug } = req.params;
+   console.log(slug);
+   switch(slug){
+          case 'upload':
+           // console.log(req.body);
+           let body = req.body;
+           //const body = await req.json();
+           try {
+            const jsonResponse = await handleUpload({
+              body,
+              req,
+              onBeforeGenerateToken: async (pathname /*, clientPayload */) => {
+                // Generate a client token for the browser to upload the file
+                // ⚠️ Authenticate and authorize users before generating the token.
+                // Otherwise, you're allowing anonymous uploads.
+         
+                return {
+                  allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+                  tokenPayload: JSON.stringify({
+                    // optional, sent to your server on upload completion
+                    // you could pass a user id from auth, or a value from clientPayload
+                  }),
+                };
+              },
+              onUploadCompleted: async ({ blob, tokenPayload }) => {
+                // Get notified of client upload completion
+                // ⚠️ This will not work on `localhost` websites,
+                // Use ngrok or similar to get the full upload flow
+         
+                console.log('blob upload completed', blob, tokenPayload);
+         
+                try {
+                  // Run any logic after the file upload completed
+                  // const { userId } = JSON.parse(tokenPayload);
+                  // await db.update({ avatar: blob.url, userId });
+                } catch (error) {
+                  throw new Error('Could not update user');
+                }
+              },
+            });
+         
+            return res.send('success');
+          } catch (error) {
+            return res.send('error');
+          }
+   }
+})
+// app.post('/api/item/:slug',(req, res) => {
+//     const { slug } = req.params;
+//     console.log(slug);
+//     console.log("newweww");
+//     switch(slug){
+//       case 'upload_file_post':
+//         console.log(req.query.filename)
+//         //console.log(req.body);
+//         //console.log(req.files);
+//         //const { searchParams } = new URL(req.url);
+//        // const filename = searchParams.get('filename');
+//         res.end(`success`);
+//         //await runMiddleware(req, res, uploadMiddleware);
+//         //console.log(req.file.buffer);
+//         //console.log(req);
+//     }
+// })
+// export const config = {
+//     api: {
+//       bodyParser: false,
+//     },
+//   };
 module.exports = app;
+// export const config = {
+//     api: {
+//         bodyParser: false,
+//         externalResolver: true,
+//     },
+// };
--- public/js/client-post.js
+++ public/js/client-post.js
@@ -1,21 +1,65 @@
-window.onload=function(){
 
+import {upload} from 'https://cdn.jsdelivr.net/npm/@vercel/blob@0.21.0/client/+esm'
+//window.onload=function(){
+ 
+  console.log(upload);
     console.log("client js loaded");
 
     const form = document.getElementById("form");
-    form.addEventListener("submit",  async function(e){
-        e.preventDefault();
-        const name = document.getElementById("veg");
-        console.log(name.value);
-        const files = document.getElementById("uploaded_file");
-        const formData = new FormData();
-        formData.append("veg", name.value);
-        formData.append("uploaded_file", files.files[0]);
-        let response = await fetch("/upload_file_post", {
-        method: 'POST',
-        body: formData
-    })
-  console.log(await response.text());
+//     form.addEventListener("submit",  async function(e){
+//         e.preventDefault();
+//         const name = document.getElementById("veg");
+//         console.log(name.value);
+//         const files = document.getElementById("uploaded_file");
+//        const data = {veg:name.value}
+//        // formData.append("uploaded_file", files.files[0]);
+//         let response = await fetch("api/item/upload_file_post", {
+//         method: 'POST',
+//         body: JSON.stringify(data)
+//     })
+//   console.log(await response.text());
+// })
+
+form.addEventListener("submit",  async function(e){
+  e.preventDefault();
+  const name = document.getElementById("veg");
+  console.log(name.value);
+  const files = document.getElementById("uploaded_file");
+ 
+  //console.log(files.files[0])
+
+   //const fd = new FormData();
+//   fd.append('veg', name.value);
+ //fd.append('file',  files.files[0]);
+//  //console.log(fd._boundary)
+
+//const fd = new FormData(form);
+//  // const urlEncodedData = new URLSearchParams(fd).toString();
+
+//  // formData.append("uploaded_file", files.files[0]);
+//   let response = await fetch("api/item/upload_file_post", {
+//   method: 'POST',
+//   body: fd
+
+// })
+const file = files.files[0];
+ const newBlob = await upload(file.name, file, {
+            access: 'public',
+            handleUploadUrl: '/api/item/upload',
+          });
+  console.log(newBlob);
+// // console.log(file);
+// const response = await fetch(
+//   `api/item/upload_file_post?filename=${file.name}`,
+//   {
+//     method: 'POST',
+//     body: fd
+//     // headers: {
+//     //   "Content-Type": "multipart/form-data"
+//     // }
+//   },
+// );
+// console.log(await response.text());
 })
 
-}
+//}
