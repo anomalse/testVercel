a7306e64afbb8c4311f74ee31c90235c0efe42de
Sabine Rosenberg <11468543+anomalse@users.noreply.github.com>
2024-02-10 16:05:19 -0500

    a different way ;)

--- api/index.js
+++ api/index.js
@@ -1,109 +1,38 @@
-/*https://vercel.com/guides/using-express-with-vercel*/
-/** to try::
- * https://jonathans199.medium.com/deploy-node-js-express-api-to-vercel-dbf4461795a5
- */
-const app = require('express')();
-const bodyParser = require('body-parser')
-//import express from 'express';
-//const app = express();
-//import { handleUpload } from '@vercel/blob/client';
-//console.log(vercelClient.handleUpload)
-const { handleUpload } =  require('@vercel/blob/client');
-
+const ImageDataURI = require('image-data-uri');
+import { put } from '@vercel/blob';
 require('dotenv').config();
 //import 'dotenv/config'
 const url = process.env.BLOB_READ_WRITE_TOKEN;
+const fs = require('fs');
 
-// parse application/json
-//app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+export default async function handler(req, res) {
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  //console.log(req.query);
 
-  switch(slug){
-    case 'varsArePassing':
-        /* GET REQUEST EX */
-       // res.end(`query: ${req.query.fruits} and ${req.query.vegs} `)
-        res.send({"fruit": req.query.fruits,"veg":req.query.vegs});
-        break;
-    
-    // case 'upload_file_post':
-    //      /* POST REQUEST EX */
-    //        res.end(`testing post 123`)
-    //     break;
-    default:
-        res.end(`no case `);
+    // const { searchParams } = new URL(req.url);
+     //const filename = searchParams.get('filename');
+    // console.log(filename)
 
+    // console.log(req.url);
+    // console.log(req.body);
+
+// declare a regexp to match the non base64 first characters
+let dataUrlRegExp = /^data:image\/\w+;base64,/;
+// remove the "header" of the data URL via the regexp
+let base64Data = req.body.file1.replace(dataUrlRegExp, "");
+// declare a binary buffer to hold decoded base64 data
+let imageBuffer = new Buffer(base64Data, "base64");
+
+
+const blob = await put(req.body.fileName, imageBuffer, {
+    access: 'public',
+  });
+  console.log(blob);
+  
+//save to BLOB :)
+// write the buffer to the local file system synchronously
+//fs.writeFileSync(req.body.fileName, imageBuffer);
+
+
+return res.send(`Hello , you just parsed the request body!`);
   }
- 
-});
-
-app.post('/api/item/:slug',async(req, res) => {
-      const { slug } = req.params;
-  //  console.log(slug);
-   switch(slug){
-          case 'upload':
-          console.log("new")
-           console.log(req.body);
-           let body = req.body;
-
-         // const body = JSON.stringify(req.body);
-          //console.log(body);
-           //console.log(req.body);
-          // console.log("again:)");
-        //   console.log(handleUpload);
-          try {
-            
-          const jsonResponse = await handleUpload({
-             body,
-             req,
-              onBeforeGenerateToken: async (pathname /*, clientPayload */) => {
-                // Generate a client token for the browser to upload the file
-                // ⚠️ Authenticate and authorize users before generating the token.
-                // Otherwise, you're allowing anonymous uploads.
-                return {
-              
-                  allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
-                  tokenPayload: JSON.stringify({
-                    // optional, sent to your server on upload completion
-                    // you could pass a user id from auth, or a value from clientPayload
-                  }),
-                };
-              },
-              onUploadCompleted: async ({ blob, tokenPayload }) => {
-                // Get notified of client upload completion
-                // ⚠️ This will not work on `localhost` websites,
-                // Use ngrok or similar to get the full upload flow
-         
-                console.log('blob upload completed', blob, tokenPayload);
-         
-                try {
-                  // Run any logic after the file upload completed
-                  // const { userId } = JSON.parse(tokenPayload);
-                  // await db.update({ avatar: blob.url, userId });
-                } catch (error) {
-                  throw new Error('Could not update user');
-                }
-              },
-            });
-         
-            //console.log(jsonResponse);
-            return res.send(jsonResponse);
-          } catch (error) {
-            console.log(error);
-            return res.send('error');
-          }
-   }
-})
-
-module.exports = app;
-//export default app;
--- public/js/client-post.js
+++ public/js/client-post.js
@@ -1,36 +1,73 @@
 
-import {upload} from 'https://cdn.jsdelivr.net/npm/@vercel/blob@0.21.0/client/+esm'
+//import {upload} from 'https://cdn.jsdelivr.net/npm/@vercel/blob@0.21.0/client/+esm'
+// import {readFile} from 'https://cdn.jsdelivr.net/npm/fs@0.0.1-security/+esm'
 //window.onload=function(){
  
-  console.log(upload);
+  //console.log(upload);
     console.log("client js loaded");
 
     const form = document.getElementById("form");
-//     form.addEventListener("submit",  async function(e){
-//         e.preventDefault();
-//         const name = document.getElementById("veg");
-//         console.log(name.value);
-//         const files = document.getElementById("uploaded_file");
-//        const data = {veg:name.value}
-//        // formData.append("uploaded_file", files.files[0]);
-//         let response = await fetch("api/item/upload_file_post", {
-//         method: 'POST',
-//         body: JSON.stringify(data)
-//     })
-//   console.log(await response.text());
-// })
 
-form.addEventListener("submit",  async function(e){
-  e.preventDefault();
-  const name = document.getElementById("veg");
-  console.log(name.value);
-  const files = document.getElementById("uploaded_file");
-const file = files.files[0];
- const newBlob = await upload(file.name, file, {
-            access: 'public',
-            handleUploadUrl: '/api/item/upload',
-          });
-  console.log(newBlob);
-  document.querySelector("#response").innerHTML = `<p><img src = ${newBlob.url} /></p>`;
+    form.addEventListener("submit",  async function(e){
+        e.preventDefault();
+        const name = document.getElementById("veg");
+        console.log(name.value);
+      const files = document.getElementById("uploaded_file");
+
+      // const response = await fetch(
+      //   `/api/?filename=${files.files[0].name}`,
+      //   {
+      //     method: 'POST',
+      //     body:files.files[0],
+      //   },
+      // );
+      
+
+       
+
+       let fr = new FileReader();
+       fr.onload = async function(){
+        console.log(fr.result);
+
+        const bodyF = new FormData();
+        let t = {file1: fr.result, fileName:files.files[0].name};
+
+
+        let response = await fetch("api/", {
+        method: 'POST',
+        headers: {
+           "Content-Type": "application/json",
+          //'Content-Type': 'application/x-www-form-urlencoded',
+          //'Content-Type':'multipart/form-data'
+          //  'Content-type':'application/octet-stream'
+        },
+        body:JSON.stringify(t)
+     })
+   console.log(await response.text());
+
+
+
+       };
+       fr.readAsDataURL(files.files[0]);
+
+       //const blob = new Blob([await fs.readFile(body.set("file1", blob, fileName))]);
+
+
+
+    
 })
+
+// form.addEventListener("submit",  async function(e){
+//   e.preventDefault();
+//   const name = document.getElementById("veg");
+//   console.log(name.value);
+//   const files = document.getElementById("uploaded_file");
+// const file = files.files[0];
+//  const newBlob = await upload(file.name, file, {
+//             access: 'public',
+//             handleUploadUrl: '/api/item/upload',
+//           });
+//   console.log(newBlob);
+//   document.querySelector("#response").innerHTML = `<p><img src = ${newBlob.url} /></p>`;
+// })
 
