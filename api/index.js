3c481dbcaed7fb0941f1093a7bb6145cd8b95dca
Sabine Rosenberg <11468543+anomalse@users.noreply.github.com>
2024-02-11 20:34:14 -0500

    testing mongo

--- api/index.js
+++ api/index.js
@@ -4,20 +4,55 @@ require('dotenv').config();
 //import 'dotenv/config'
 const url = process.env.BLOB_READ_WRITE_TOKEN;
 const fs = require('fs');
+import {setup,db_insert_default,db_insert_one } from "./setup_mongo.js";
+//let connectionISSet = false;
 
+
+// try {
+//   console.log("test123");
+//   setup();
+// } catch {
+//   console.warn('Database is not ready yet. Skipping seeding...');
+// }
 
 export default async function handler(req, res) {
 console.log("here");  
+//if(connectionISSet===false){
+  console.log('tests');
+  // const message = await setup();
+
+  // if (message) {
+  //   res.status(500).json({
+  //     error: { message }
+  //   });
+  // } else {
+  //  // connectionISSet=true;
+  //  // await res.revalidate(`/`);
+  //   //res.status(200).send('ok.'); //don't do that otherwise will not continue
+  //   console.log("had a connection");
+
+  // }
+//}
+
+//}
+
+
 console.log(req.query)
+
 if(Object.keys(req.query).length !== 0){
   console.log("get");
+  
   if(req.query.type ==="start"){
+    let result = await db_insert_one(req.query.fruits,req.query.vegs);
+    //let result = await db_insert_default();
+    console.log(result);
     res.send({"fruit": req.query.fruits,"veg":req.query.vegs});
 
   }
 
 }
 else{
+  console.log("hererere");
   // declare a regexp to match the non base64 first characters
 let dataUrlRegExp = /^data:image\/\w+;base64,/;
 // remove the "header" of the data URL via the regexp
@@ -39,5 +74,4 @@ const blob = await put(req.body.fileName, imageBuffer, {
 return res.send(`Hello , you just parsed the request body!`);
 
 }
-
-  }
+}
---
+++ api/setup_mongo.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+import {MongoClient} from 'mongodb';
+
+const db_find_some = function(){}
+const db_update_one = function(){}
+
+const db_insert_one = function(in_veg,in_fruit) {
+  return new Promise (async(resolve, reject) => {
+  let client;
+  let record = {fruit: in_fruit,veg:in_veg}
+  
+  try {
+    client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    //console.log(client);
+
+    const insert = await client
+      .db('vercel')
+      .collection('favorites')
+      .insertOne(record);
+
+    if (insert.acknowledged) {
+     resolve('Successfully inserted a record');
+    }
+  } catch (error) {
+    reject ('Database is not ready yet');
+  } finally {
+    if (client) {
+      await client.close();
+      //resolve('done');
+    }
+  }
+})
+}
+
+const db_insert_default = function() {
+  return new Promise (async (resolve, reject) => {
+  let client;
+  const testRecords = [
+    {fruit: "watermelon",veg:"carrots"},
+    {fruit: "apple",veg:"celery"}
+  ]
+  try {
+    console.log("we are here");
+    client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    const insert = await client
+      .db('vercel')
+      .collection('favorites')
+      .insertMany(testRecords);
+
+    if (insert.acknowledged) {
+     resolve('Successfully inserted records');
+    }
+  } catch (error) {
+    reject ('Database is not ready yet');
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+})
+}
+
+const setup = async () => {
+    let client;
+  try {
+    client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+  
+    const hasData = await client
+        .db('vercel')
+        .collection('favorites')
+        .countDocuments();
+  
+      if (hasData) {
+        console.log('Database already exists with data');
+        client.close();
+        return;
+      }
+  
+  }
+  catch {
+    console.warn('Database is not ready yet. Skipping seeding...');
+  }
+  
+
+  }
+  
+  export { setup, db_insert_default,db_insert_one };
+  
+  
